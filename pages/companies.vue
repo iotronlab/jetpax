0e2e1152f3e43b16f6b6de221032c7367eef5417
iotronlab <iotronlabspvt@gmail.com>
2021-05-10 20:42:56 +0530

    vmodel

--- pages/companies.vue
+++ pages/companies.vue
@@ -1,4 +1,3 @@
-
 <template>
   <v-container>
     <h1 class="text-h4">
@@ -6,7 +5,7 @@
             <span class="">that</span><br /> -->
       Why choose <span class="font-weight-bold"> JETPAX</span>?
     </h1>
-    <h4 class="subtitle-1">
+    <h4 class="subtitle-1 mb-4">
       Grow your brand with dedicated marketing campaigns run by our influencer
       network. We approach every task intending to formulate and build
       strategies and campaigns tailor-made for our partners, aiming to push
@@ -15,10 +14,35 @@
       plan creative and budget-friendly influencer campaign that reaches the
       right audience.
     </h4>
+    <h1 class="text-h4">
+      <!-- <span class="light-green--text">ideas</span>
+            <span class="">that</span><br /> -->
+      Contact<span class="font-weight-bold">US.</span>
+    </h1>
+    {{ name }}<br />{{ email }}
+    <v-text-field
+      label="Name"
+      v-model="name"
+      placeholder="Enter your name"
+    ></v-text-field>
+    <v-text-field
+      label="Email"
+      v-model="email"
+      placeholder="Enter your email"
+    ></v-text-field>
+    <v-row no-gutters></v-row>
   </v-container>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      name: 'Anik',
+      business_name: 'Iotron',
+      email: 'anikadhikary94@gmail.com',
+    }
+  },
+}
 </script>
 
