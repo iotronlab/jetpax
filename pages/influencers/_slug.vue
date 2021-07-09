0dde04fed4338874e029a3848a0aec3a99f53375
iotronlab <iotronlabspvt@gmail.com>
2021-07-09 18:22:42 +0530

    update

--- pages/influencers/_slug.vue
+++ pages/influencers/_slug.vue
@@ -304,18 +304,29 @@
         </v-row>
         <!-- <MiniProfile :creator="creator" /> --></v-container
       >
-      <h1>{{ creator.name }}<span>'s Services</span></h1>
-      <v-card v-for="(service, i) in creator.services" :key="i">
-        <v-card-title class="text-h5"> {{ service.name }}</v-card-title>
-
-        <v-card-subtitle
-          >Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quo
-          dolores, earum maiores dolorem quod velit voluptas accusamus nostrum
-          minus quaerat dolor totam illum voluptate itaque laboriosam
-          distinctio! Asperiores, hic?</v-card-subtitle
+      <h1 class="text-h6">{{ creator.name }}'s Services</h1>
+      <v-row no-gutters
+        ><v-col
+          v-for="(service, i) in creator.services"
+          :key="i"
+          cols="12"
+          lg="3"
+          md="6"
+          sm="6"
         >
-        <v-divider></v-divider>
-      </v-card>
+          <v-card class="ma-1" rounded="lg">
+            <v-card-title class="text-h5"> {{ service.name }}</v-card-title>
+
+            <v-card-subtitle
+              >Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+              quo dolores, earum maiores dolorem quod velit voluptas accusamus
+              nostrum minus quaerat dolor totam illum voluptate itaque
+              laboriosam distinctio! Asperiores, hic?
+              <h2 class="text-h6">₹ {{ service.rate }}</h2></v-card-subtitle
+            >
+          </v-card></v-col
+        ></v-row
+      >
     </section>
   </v-container>
 </template>
